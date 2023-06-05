69ffa76743cae36814001a3cc3b2a866a644b1c0
ijp <isaacpeterson2017@gmail.com>
2023-06-04 22:46:07 -0500

    added automatic tagging program. will add vim command to invoke it within editor

---
+++ src/taggit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Generate tags for the entire repo
+
+root="$(git rev-parse --show-toplevel 2>/dev/null)"
+forg="$root/.gitignore" # https://git-scm.com/docs/gitignore (persistant)
+list="$root/taggit" # file containing list of files to tag (temporary)
+tags="$root/tags" # file containing tags (persistant)
+
+if [[ -n "$root" ]]; then
+  git ls-files --recurse-submodules $root > "$list"
+  ctags -L "$list" -f "$tags"
+  rm "$list"
+  echo "$tags created"
+  
+  if [[ ! -f "$forg" ]]; then
+	touch "$forg"
+	echo "$forg created"
+  fi
+  if !(grep -a "$tags" "$forg"); then
+    echo "$tags" >> "$forg"
+    echo "$tags added to $forg"
+  fi
+else
+  echo "error: not in a git repo"
+fi
+
